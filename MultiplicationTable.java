fde099783ef364aa74367a00b993e912eb8b3e97
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-04-07 19:36:04 -0400

    Added files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,9 @@
+public class MultiplicationTable {
+	public static void main(String[] args){
+		for(int i=0;i<=12;i++){
+			for(int j=0;j<=12;j++){
+				System.out.printf("%d x %d = %d\n",i,j,i*j);
+			}
+		}
+	}
+}
